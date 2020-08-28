d64c9718a6d2ea119e0f1eba9fb96cd34561ba73
bakteriekultur <jakoblunden@live.se>
2020-08-28 19:25:31 +0200

    Adding pre-existing files

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+int getKey(int lastKey){
+  HANDLE hstdin;
+  DWORD mode, timer;
+  hstdin = GetStdHandle(STD_INPUT_HANDLE);
+  GetConsoleMode(hstdin, &mode);
+  SetConsoleMode(hstdin, ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
+  timer = GetTickCount() + 50;
+  while(!kbhit()){
+    while(timer > GetTickCount()){
+      if(kbhit()){
+        int button = std::cin.get();
+      switch(button){
+        case 119: lastKey = 1; break;
+        case 97: lastKey = 2;break;
+        case 115: lastKey = 3;break;
+        case 100: lastKey = 4;break;
+      }
+      }
+    }
+      SetConsoleMode(hstdin, mode);
+      return lastKey;
+  }
+  SetConsoleMode(hstdin, mode);
+  return lastKey;
+}
+int main(){
+  DWORD written = 0;
+  short x,y = 0;
+  short oX,oY;
+  const char *title = "Testing"; 
+  const char *write = "abc"; 
+  int lastKey = 3;
+  char *input = new char[5];
+  for(int i=0; i<5; i++){
+    if(i%2==0)input[i]='a';
+    else input[i]='b';
+  }
+  HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+  SetConsoleActiveScreenBuffer(hConsole);
+  if(SetConsoleTitle(title))SetConsoleTitle(title);
+  while(true){
+      WriteConsoleOutputCharacter(hConsole, input, 1, {x,y}, &written);
+      oX = x;
+      oY = y;
+      lastKey = getKey(lastKey);
+      switch(lastKey){
+        case 1: y--; break;
+        case 2: x-=2; break;
+        case 3: y++; break;
+        case 4: x+=2; break;
+          }
+      WriteConsoleOutputCharacter(hConsole, " ", 1, {oX,oY}, &written);
+      std::cout << x << " " << y << std::endl;
+  }
+  return 0;
+}
